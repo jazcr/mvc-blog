6ec8d6249e1a562ef7202a4fb7f5442bc0cbc3da
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-08-08 19:30:49 -0600

    login page displaying

--- server.js
+++ server.js
@@ -3,19 +3,41 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
-// const session = require('express-session');
+const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3304;
 
-//need to add sessions
+const SequelizeStore = require('express-session-sequelize')(session.Store);
+
+//add sessions
+const sess = {
+    secret: 'cooookies',
+    cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+    },
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
